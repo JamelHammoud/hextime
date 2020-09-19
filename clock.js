ee6a284b3f100eb7cb8b7405979f907fe68ecd0d
partyvaper <58113606+partyvaper@users.noreply.github.com>
2020-09-20 01:57:31 +0300

    forgot to remove, helpful comment

--- clock.js
+++ clock.js
@@ -1,6 +1,5 @@
 setInterval(function() {
-    today = new Date();
-    hex = '#' + new Date().toISOString().substr(11, 8).replace(/:/g, '');
-    document.querySelector("#clock").innerHTML = hex;
+    var hex = '#' + new Date().toISOString().substr(11, 8).replace(/:/g, '');
+    document.getElementById('clock').innerHTML = hex;
     document.body.style.backgroundColor = hex;
-}, 500);
+}, 500); // 0.5s
