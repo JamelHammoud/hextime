eda50ec1cf00232458be75e7d1ec4ca2725d11e5
partyvaper <58113606+partyvaper@users.noreply.github.com>
2020-09-20 01:53:52 +0300

    shorter code to get hours, minutes, seconds

--- clock.js
+++ clock.js
@@ -1,6 +1,6 @@
 setInterval(function() {
     today = new Date();
-    hex = "#" + (today.getHours() < 10 ? '0' : '') + today.getHours() + "" + (today.getMinutes() < 10 ? '0' : '') + today.getMinutes() + "" + (today.getSeconds() < 10 ? '0' : '') + today.getSeconds();
+    hex = '#' + new Date().toISOString().substr(11, 8).replace(/:/g, '');
     document.querySelector("#clock").innerHTML = hex;
     document.body.style.backgroundColor = hex;
 }, 1)
