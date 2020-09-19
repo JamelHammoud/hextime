27d4bb1f7f9b23b05e84deee959b0e201701a813
partyvaper <58113606+partyvaper@users.noreply.github.com>
2020-09-20 01:55:48 +0300

    unneccesary small interval
    no need to check every 1 ms, as values will change only each 1000 ms (each 1 second), so 500 ms is kind of safe way to do this.

--- clock.js
+++ clock.js
@@ -3,4 +3,4 @@ setInterval(function() {
     hex = '#' + new Date().toISOString().substr(11, 8).replace(/:/g, '');
     document.querySelector("#clock").innerHTML = hex;
     document.body.style.backgroundColor = hex;
-}, 1)
+}, 500);
